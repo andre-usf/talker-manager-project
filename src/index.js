98bd1a57764de696474922e4fcf78e5bcc4bd900
andre-usf <andrefretta@gmail.com>
2023-02-06 22:11:27 -0300

    feat: cria endpoint PUT /talker/:id - completa requisito 06.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const { getAllTalkers, getTalkerById, writeFile, getLastTalkerId } = require('./talkerManager');
+const { getAllTalkers, getTalkerById, writeFile, 
+  getLastTalkerId, editFile } = require('./talkerManager');
 const generateToken = require('./generateToken');
 const validateLogin = require('./middlewares/validateLogin');
 const validateToken = require('./middlewares/validateToken');
@@ -48,4 +49,15 @@ validateAge, validateTalk, validateTalkRate, async (req, res) => {
   const talker = { name, age, id, talk: { watchedAt, rate } };
   await writeFile(talker);
   return res.status(201).json({ age, id, name, talk: { watchedAt, rate } });
+});
+
+app.put('/talker/:id', validateToken, validateName, 
+validateAge, validateTalk, validateTalkRate, async (req, res) => {
+  const { id } = req.params;
+  const numberId = Number(id);
+  const { name, age, talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const talker = { name, age, id: numberId, talk: { watchedAt, rate } };
+  await editFile(talker, numberId);
+  return res.status(200).json({ age, id: numberId, name, talk: { watchedAt, rate } });
 });
--- src/talkerManager.js
+++ src/talkerManager.js
@@ -29,9 +29,25 @@ const writeFile = async (talker) => {
   return file;
 };
 
+const editFile = async (talker, id) => {
+  const originalFileArray = await readFile();
+
+  for (let index = 0; index < originalFileArray.length; index += 1) {
+    if (originalFileArray[index].id === id) {
+      originalFileArray[index].name = talker.name;
+      originalFileArray[index].age = talker.age;
+      originalFileArray[index].talk.watchedAt = talker.talk.watchedAt;
+      originalFileArray[index].talk.rate = talker.talk.rate;
+    }
+  }
+  const file = await fs.writeFile('./src/talker.json', JSON.stringify(originalFileArray));
+  return file;
+};
+
 module.exports = {
   getAllTalkers,
   getTalkerById,
   getLastTalkerId,
   writeFile,
+  editFile,
 };
